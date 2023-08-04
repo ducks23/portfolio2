60a2c3ba18d969bcefc099b2047d2022888bc879
Jesse Leonard <jesse1517@gmail.com>
2023-08-04 12:14:22 -0500

    fixe env variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_GRAPHCMS_ENDPOINT: process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT,
+    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
+    CHANNEL_ID: process.env.CHANNEL_ID,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
