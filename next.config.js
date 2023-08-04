620f40545c21faba161ef931f4d99883b5ef57f1
Jesse Leonard <jesse1517@gmail.com>
2023-08-04 12:52:05 -0500

    fix package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "build",
   env: {
     NEXT_PUBLIC_GRAPHCMS_ENDPOINT: process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT,
     YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
