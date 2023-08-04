0bda18b5da3305936cf5f995c42026734d147ee1
Jesse Leonard <jesse1517@gmail.com>
2023-08-04 13:17:34 -0500

    fix package.json

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-//import Car from "@/components/animation/Car";
+import Car from "@/components/animation/Car";
 import Work from "@/components/Work";
 import VideoCard from "@/components/VideoCard";
 export default function Home() {
   return (
     <div className=" dark:bg-gray-800 font-mono">
-      {/* <Car /> */}
+      <Car />
       <Work />
       <VideoCard />
     </div>
