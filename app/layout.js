55564f8f33c7390aadf924e2138a63d9fd25394a
Jesse Leonard <jesse1517@gmail.com>
2023-09-13 16:16:19 -0500

    added google tag

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-62XWJEBBYK"
+        ></Script>
+        <Script>
+          {`window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-62XWJEBBYK');
+  `}
+        </Script>
+
         <Navbar />
 
         {children}
